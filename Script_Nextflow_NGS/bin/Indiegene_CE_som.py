668fc030f6d2e8913b04d17567cd6edd96f94f6f
pragati-29 <131236738+pragati-29@users.noreply.github.com>
2025-03-23 01:17:37 +0530

    Update Indiegene_CE_som.py

--- Script_Nextflow_NGS/bin/Indiegene_CE_som.py
+++ Script_Nextflow_NGS/bin/Indiegene_CE_som.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 import pandas as pd
 import subprocess
 import argparse
@@ -32,26 +32,26 @@ for test_name, capr_kit, proj_id, appsession_name, bed_id, liq_tm, vc_af_call, v
     file1["vc_type"],
     file1["Sample_Type"],
 ):
-    if "somatic" in germ_som and "INDIEGENE" in test_name and "CE" in capr_kit and "DNA" in sample_type:
-        biosamp_CE_som = ", ".join(map(str, [grouped_samp.loc["CE", "somatic"]]))
-        command_bs_launch = (
-            f'bs launch application -n "DRAGEN Enrichment" '
-            f"--app-version 3.9.5 -o project-id:{proj_id} "
-            f"-o app-session-name:{appsession_name} -l {appsession_name} "
-            f"-o vc-type:{vc_type} -o ht-ref:hg19-altaware-cnv-anchor.v8 "
-            f"-o fixed-bed:custom -o target_bed_id:{bed_id} "
-            f"-o input_list.sample-id:biosamples/{biosamp_CE_som} "
-            f"-o picard_checkbox:1 -o liquid_tumor:{liq_tm} "
-            f"-o af-filtering:1 -o vc-af-call-threshold:{vc_af_call} "
-            f"-o vc-af-filter-threshold:{vc_af_filt} -o sv_checkbox:1 "
-            f"-o sq-filtering:1 -o tmb:1 -o vc-hotspot:27723066652 "
-            f"-o {noise_base} -o vcf-site-filter:1 -o cnv_checkbox:1 "
-            f"-o cnv_ref:1 -o cnv_segmentation_mode:cbs -o cnv-filter-qual:50.0 "
-            f"-o {cnv_base} -o cnv_gcbias_checkbox:1 -o hla:1 -o nirvana:1 "
-            f'-o commandline-disclaimer:true -o arbitrary:"--read-trimmers:adapter --trim-adapter-read1" '
-            f"-o additional-file:25600057590 -o automation-sex:unknown"
-        )
-        print(command_bs_launch)
-        bs_launch_run = subprocess.run(command_bs_launch, shell=True, capture_output=True, text=True)
-        print("STDOUT:", bs_launch_run.stdout)
-        print("STDERR:", bs_launch_run.stderr)
+    if "somatic" in germ_som and "INDIEGENE" in test_name and "DNA" in sample_type:
+        if capr_kit == "CE":
+            biosamp_CE_som = ", ".join(map(str, [grouped_samp.loc["CE", "somatic"]]))
+            command_bs_launch = (
+                f'bs launch application -n "DRAGEN Enrichment" '
+                f"--app-version 3.9.5 -o project-id:{proj_id} "
+                f"-o app-session-name:{appsession_name} -l {appsession_name} "
+                f"-o vc-type:{vc_type} -o ht-ref:hg19-altaware-cnv-anchor.v8 "
+                f"-o fixed-bed:custom -o target_bed_id:{bed_id} "
+                f"-o input_list.sample-id:biosamples/{biosamp_CE_som} "
+                f"-o picard_checkbox:1 -o liquid_tumor:{liq_tm} "
+                f"-o af-filtering:1 -o vc-af-call-threshold:{vc_af_call} "
+                f"-o vc-af-filter-threshold:{vc_af_filt} -o sv_checkbox:1 "
+                f"-o sq-filtering:1 -o tmb:1 -o vc-hotspot:27723066652 "
+                f"-o {noise_base} -o vcf-site-filter:1 -o cnv_checkbox:1 "
+                f"-o cnv_ref:1 -o cnv_segmentation_mode:cbs -o cnv-filter-qual:50.0 "
+                f"-o {cnv_base} -o cnv_gcbias_checkbox:1 -o hla:1 -o nirvana:1 "
+                f'-o commandline-disclaimer:true -o arbitrary:"--read-trimmers:adapter --trim-adapter-read1" '
+                f"-o additional-file:25600057590 -o automation-sex:unknown"
+            )
+            bs_launch_run = subprocess.run(command_bs_launch, shell=True, capture_output=True, text=True)
+            print("STDOUT:", bs_launch_run.stdout)
+            print("STDERR:", bs_launch_run.stderr)
