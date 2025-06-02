f0d242a45720e041322cd91a7d033be87b4b7562
Nilesh Mukherjee <MaxSimonNm@users.noreply.github.com>
2025-06-02 16:01:31 +0530

    Refactor setup script for improved path management and execution permissions

--- Script_Nextflow_NGS/bin/setup.sh
+++ Script_Nextflow_NGS/bin/setup.sh
@@ -3,24 +3,30 @@
 # Ensure the script runs from the repo root
 cd "$(dirname "$0")"
 
-# Ensure bin/ exists
-mkdir -p bin
+# Define base directory
+BIN_DIR="/home/bioinfo/Nilesh/NGS3_test/Nextflow_Downstream/Script_Nextflow_NGS/bin"
 
 # Grant execute permission to all Python files in bin/
-chmod +x /home/bioinfoa/Pragati/Nextflow_NGS3Pipeline/Script_Nextflow_NGS/bin/*.py #chmod +x *
+chmod +x "$BIN_DIR"/*
 
 # Fix line endings for Python scripts in bin/
-sed -i 's/\r$//' /home/bioinfoa/Pragati/Nextflow_NGS3Pipeline/Script_Nextflow_NGS/bin/*.py 2>/dev/null || true
+find "$BIN_DIR" -name "*.py" -exec sed -i 's/\r$//' {} \;
 
 echo "Setup complete! Python scripts in bin/ are now executable."
 
-export FREEC_PATH="/home/bioinfoa/Programs/FREEC-11.6/src/freec"
-export BEDTOOLS_PATH="/home/bioinfoa/Programs/bedtools.static.binary"
+export FREEC_PATH="/home/bioinfo/Programs_Assets/FREEC-11.6/src/freec"
+export BEDTOOLS_PATH="/home/bioinfo/Programs_Assets/bedtools.static.binary"
 export BED_FILE_Fev2_CDS="/home/bioinfoa/Programs/NGS3pipeline/FEV2F2both/CNV/FE_V2_CDS.bed"
-export cnv_config="/home/bioinfoa/Pragati/Nextflow_NGS3Pipeline/Script_Nextflow_NGS/bin/cnv_config_somatic.pl"
-export chrLenFile="/home/bioinfoa/Programs/NGS3pipeline/FEV2F2both/CNV/my_genome.fa.fai"
-export chrFiles="home/bioinfoa/Programs/files_for_control_freec/chromFa"
-export sambamba="/usr/bin/sambamba"
-export COMMON_FILE_DIR="/home/bioinfoa/Programs/Freebayes_Test_Runs/Common_files/"
-export FREEBAYES_PATH="/usr/bin/freebayes"
+export cnv_config="/home/bioinfo/Nilesh/NGS3_test/Nextflow_Downstream/Script_Nextflow_NGS/bin/cnv_config_somatic.pl"
+export chrLenFile="/home/bioinfo/Nilesh/NGS3_test/Nextflow_Downstream/Script_Nextflow_NGS/bin/4bc_NGS3Pipeline_FEV2F2both-main/CNV/my_genome.fa.fai"
+export chrFiles="/home/bioinfo/Programs_Assets/files_for_control_freec/chromFa"
+export sambamba="/home/bioinfo/miniconda3/bin/sambamba"
+export COMMON_FILE_DIR="/home/bioinfo/Programs_Assets/files_for_control_freec/Common_files/"
+export python_bam_ratio="$BIN_DIR/bam_ratio_cna_FEV2F2both_V3.py"
+export Ambiliz="$BIN_DIR/AmpliZ_cov_V3.py"
+export Ambiliz_folder="$BIN_DIR/4bc_NGS3Pipeline_FEV2F2both-main/AmpliZ_COV"
+export CE_gene_list="$BIN_DIR/gene_list_CE.txt"
+export bam_ratio_CE="$BIN_DIR/bam_ratio_cna_CE_V4.py"
+export mosdepth="/home/bioinfo/miniconda3/envs/nf_ngs3pipeline_env/bin/mosdepth"
+export FREEBAYES_PATH="/home/bioinfo/miniconda3/envs/nf_ngs3pipeline_env/bin/freebayes"
 export IGV="home/bioinfoa/Programs/IGV_Linux_2.9.2_WithJava/IGV_Linux_2.9.2/igv"
