7bc4fee8a585678e51fc3ffa354eeb7a6778527b
Nilesh Mukherjee <MaxSimonNm@users.noreply.github.com>
2025-06-17 15:25:07 +0530

    Update CNV_somatic_FeV2.sh

--- Script_Nextflow_NGS/bin/CNV_somatic_FeV2.sh
+++ Script_Nextflow_NGS/bin/CNV_somatic_FeV2.sh
@@ -26,30 +26,33 @@ echo "Location: $output_dir"
 echo "Directory: $cnv_dir"
 echo "Sample file: $csv_file"
 
-# Create directory if it doesn't exist
 mkdir -p "$cnv_dir"
-#basemount-cmd refresh "${output_dir}/basespace"
 
-# Extract Sample ID (15th column) and Project Name (5th column)
-awk -F',' 'BEGIN {OFS=","} {if (NR>1) print $(NF-2),$4}' "$csv_file" > "$cnv_dir/list.txt"
+# Extract only TARGET_FIRST samples with Sample ID and Project Name
+awk -F',' 'BEGIN {OFS=","} NR > 1 && $1 == "TARGET_FIRST" {print $(NF-2), $4}' "$csv_file" > "$cnv_dir/list.txt"
 
 input="$cnv_dir/list.txt"
 cd "$cnv_dir" || { echo "Failed to change directory to $cnv_dir"; exit 1; }
 
-# Loop through each line in the extracted list
+# Check if there are any TARGET_FIRST samples
+if [ ! -s "$input" ]; then
+    echo "No TARGET_FIRST samples found. Exiting."
+    exit 0
+fi
+
+# Loop through each TARGET_FIRST sample
 while IFS=',' read -r sample_id project_name; do
     echo "Processing Sample: $sample_id, Project: $project_name"
     
     bam_path="$output_dir/basespace/Projects/${project_name}/AppResults/${sample_id}/Files/${sample_id}.bam"
-    bed_file=$(ls $output_dir/basespace/Projects/${project_name}/AppResults/${sample_id}/Files/*.bed | awk -F/ '!($NF ~ /'${sample_id}'/) { print $0 }')
+    bed_file=$(ls "$output_dir/basespace/Projects/${project_name}/AppResults/${sample_id}/Files/"*.bed | awk -F/ '!($NF ~ /'"${sample_id}"'/) { print $0 }')
     echo "bed file: $bed_file"
-
     echo "BAM path: $bam_path"
    
     mkdir -p "$sample_id"
 
     echo "########## Starting config preparation #################"
-    perl "$cnv_config" "$bam_path" "$bed_file" $sample_id $chrLenFile $chrFiles $sambamba
+    perl "$cnv_config" "$bam_path" "$bed_file" "$sample_id" "$chrLenFile" "$chrFiles" "$sambamba"
     echo "########## Config file prepared #######################"
 
     echo "########## Starting CNV Control-FREEC ##################"
@@ -60,7 +63,7 @@ while IFS=',' read -r sample_id project_name; do
     cd "$sample_id"
 
     cp "$python_bam_ratio" .
-    python3 "$(basename "$python_bam_ratio")" $bed_file
+    python3 "$(basename "$python_bam_ratio")" "$bed_file"
 
     mkdir -p MS_Cov
     cp "$Ambiliz" .
@@ -76,4 +79,3 @@ while IFS=',' read -r sample_id project_name; do
 done < "$input"
 
 echo "################## ALL FILES ARE DONE ###################"
-
